0130db56003019b34c8451af6cd189976dc666ba
prasish07 <prasishshrestha0099@gmail.com>
2026-06-14 02:16:00 +0545

    refactor: update src/features imports to use path aliases

--- src/features/links/data/links.repo.ts
+++ src/features/links/data/links.repo.ts
@@ -1,14 +1,14 @@
-import db from "../../../db/client";
+import db from "@/db/client";
 import { Link } from "../types";
 
 export const getAllLinks = async (): Promise<Link[]> => {
   return db.getAllAsync<Link>(
-    "SELECT * FROM links where is_archived = 0 ORDER BY created_at DESC",
+    "SELECT * FROM links where is_archived = 0 ORDER BY created_at DESC"
   );
 };
 
 export const insertLink = async (
-  link: Omit<Link, "created_at" | "updated_at" | "is_archived">,
+  link: Omit<Link, "created_at" | "updated_at" | "is_archived">
 ): Promise<void> => {
   await db.runAsync(
     `INSERT INTO links (id, url, title, description, thumbnail_url, site_name, favicon_url, note, group_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
@@ -22,6 +22,6 @@ export const insertLink = async (
       link.favicon_url ?? null,
       link.note ?? null,
       link.group_id ?? null,
-    ],
+    ]
   );
 };
